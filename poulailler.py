ad5131e881f09b51e57d192783a42c12be6cddbd
ErwanB <erwanbroquaire@hotmail.com>
2021-01-17 21:42:09 +0100

    Tester différents cas de position et d'heure + meilleure gestion du timezone

--- poulailler.py
+++ poulailler.py
@@ -11,18 +11,23 @@ from datetime import datetime, timedelta
 import pytz
 
 print("""
-                  ,.
-                 (\(\)
- ,_              ;  o >
-  {`-.          /  (_)
-  `={\`-._____/`   |
-   `-{ /    -=`\   |
-    `={  -= = _/   /
-       `\  .-'   /`
-        {`-,__.'===,_
-        //`        `\\
-       //
-      `\=
+      ,~.
+   ,-'__ `-,
+  {,-'  `. }              ,')
+ ,( a )   `-.__         ,',')~,
+<=.) (         `-.__,==' ' ' '}
+  (   )                      /)
+   `-'\   ,                    )
+       |  \        `~.        /
+       \   `._        \      /
+        \     `._____,'    ,'
+         `-.             ,'
+            `-._     _,-'
+                77jj'
+               //_||
+            __//--'/`          
+          ,--'/`  '
+
 """)
 
 
@@ -35,18 +40,28 @@ print("le poulailler se situe ici (lat:{}, long:{})".format(latitude, longitude)
 sun = Sun(latitude, longitude)
 
 
+
+
+
 def monter():
 	print("On monte()")
+	print("on verifie que la position n'est pas aberante")
 	with open("position.txt","r") as mon_fichier :
 		if mon_fichier.readlines()[0]!="bas":
-			raise Exception("On me demande de descendre alors que je ne suis pas en bas !")
+			raise Exception("On me demande de monter alors que je ne suis pas en bas !")
+		else:
+			print("position ok")
+		mon_fichier.close()
+
 	with open("position.txt","w") as mon_fichier :
+		print("on indique dans le fichier que la porte est en train de monter")
 		mon_fichier.write("monte")
 	sleep(1.0)
 	with open("position.txt","w") as mon_fichier :
+		print("on indique dans le fichier que la porte est en haut")
 		mon_fichier.write("haut")
 	return
-
+	
 
 def descendre():
 	print("On descend()")
@@ -66,14 +81,18 @@ def descendre():
 		print("on indique dans le fichier que la porte est en bas")
 		mon_fichier.write("bas")
 	return
+
+
+
+
+
 	
 
+############################################
 
+#         MAIN
 
-
-
-
-
+############################################
 
 # Cette boucle est parcourue toute les minutes
 # l'heure de leve/couche de soleil est recalculee
@@ -95,13 +114,10 @@ while 1:
 	today_ss += timedelta(hours=elarg_heures,minutes=elarg_minutes)
 
 	# line de code de test pour modifier la plage horaire
-	#today_ss -= timedelta(hours=3,minutes=0)
+#	today_ss += timedelta(hours=5,minutes=0)
 
 	print("On leve la porte a {} et on ferme a {}".format(today_sr.strftime('%H:%M'), today_ss.strftime('%H:%M')))
-	# Attention ici dattime.now() suppose que l'heure du system est UTC alors que c'est deja Europe/Paris
-	# La librairie suntime quant a elle ne s'occupe pas de l'heure system
-	# Je corrige donc cette disymetrie en retirant une heure pour qu'il ne compte pas deux fois le decalage horaire du a la TZ !
-	now = datetime.now().replace(tzinfo=pytz.utc).astimezone(pytz.timezone(timezone)) - timedelta(hours=1)
+	now = datetime.now().replace(tzinfo=pytz.timezone(timezone))
 	current_time = now.strftime("%H:%M")
 	print("Actuellement il est : ", current_time)
 
@@ -119,15 +135,15 @@ while 1:
 	if ((today_sr < now) and (now < today_ss)):
 		print("On est dans la plage horaire, la porte doit etre en haut")
 		if position != "haut":
-			monter()
 			print("on monte la porte")
+			monter()
 		else :
 			print("la porte est deja dans la bonne position")
 	if ((now <= today_sr) or (today_ss <= now)):
 		print("On n'est pas dans la plage horaire, la porte doit etre en bas")
 		if position != "bas":
-			descendre()
 			print("on descend la porte")
+			descendre()
 		else :
 			print("la porte est deja dans la bonne position")
 
